1343094585bf1e1f3ce0654bcd9b9ddce2c6fbdd
robinstrike <68581793+robinstrike@users.noreply.github.com>
2020-07-25 15:12:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,31 +5,33 @@ import os
 import shutil
 
 
-red = praw.Reddit(client_id="_4W54SFBNZJwAA",
-                  client_secret="jiQj7rsZeozEquzDl9NjudQn-Z8",
-                  password="Live-in69",
-                  user_agent="testscript by /u/yardley_process",
-                  username="yardley_process")
+def scrapeimgs(subredditname, count):
+    red = praw.Reddit(client_id="_4W54SFBNZJwAA",
+                      client_secret="jiQj7rsZeozEquzDl9NjudQn-Z8",
+                      password="Live-in69",
+                      user_agent="testscript by /u/yardley_process",
+                      username="yardley_process")
 
-subred = red.subreddit("ImaginarySliceOfLife")
+    subRed = red.subreddit(subredditname)
 
-os.mkdir(os.path.join(os.getcwd(), subred.display_name))
+    os.mkdir(os.path.join(os.getcwd(), subRed.display_name))
 
-for each in subred.hot(limit = 2):
-    # print(each.url)
+    for each in subRed.hot(limit = count):
 
-    isImg = match(r".*png|.*jpg*.", each.url)
+        isImg = match(r".*\.png.*|.*\.jpg.*", each.url)
 
-    # print(isImg)
+        if isImg is None:
+            if match(r"https://imgur\.com", each.url):
+                each.url = str(each.url).replace("imgur", "i.imgur") + ".jpg"
+                isImg = True
+                
 
-    if isImg:
-        r = get(each.url, stream = True)
-        # print(type(r))
-        # print(r)
-        # print(r.raw)
-        if r.status_code == 200:
-            r.raw.decode_content = True
-            file = open(str(each), "wb")
-            shutil.copyfileobj(r.raw, file)
-            file.close()
+        if isImg:
+            r = get(each.url, stream = True)
+            if r.status_code == 200:
+                r.raw.decode_content = True
+                file = open(os.path.join(os.getcwd(), 
+                                         subRed.display_name, str(each) + ".jpg"), "wb")
+                shutil.copyfileobj(r.raw, file)
+                file.close()
 
