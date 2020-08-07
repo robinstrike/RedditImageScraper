bc4845a04ffa216a93e430e497703856b4a680f8
robinstrike <68581793+robinstrike@users.noreply.github.com>
2020-08-07 22:40:51 +0530

    Update ris.py

--- ris.py
+++ ris.py
@@ -1,4 +1,14 @@
 import app
 
-# parameters are name of subreddit and number of images 
-app.scrapeimgs("vaporwaveAesthetics", 200) 
+"""
+app.scrape_content("subreddit", count, media)
+
+subreddit : name of subreddit
+count : number of posts to grab (default = 25)
+media : 1 for pics (default)
+media : 2 for text
+media : 3 for urls
+"""
+
+app.scrape_content(subredditname = "writingprompts", count = 10, media = 2)
+
