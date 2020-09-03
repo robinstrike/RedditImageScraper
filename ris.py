086e052331d2af29d98a8d4bd05d4f7763922f6a
robinstrike <68581793+robinstrike@users.noreply.github.com>
2020-09-03 16:09:51 +0530

    Update ris.py

--- ris.py
+++ ris.py
@@ -8,6 +8,7 @@ count : number of posts to grab (default = 25)
 media : 1 for pics (default)
 media : 2 for text
 media : 3 for urls
+media : 4 for gifs
 """
 
 app.scrape_content(subredditname = "writingprompts", count = 10, media = 2)
